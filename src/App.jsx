f7b90a1afb0990bdc89f8126bf1451d114cdfce0
JoseOrdaz <joseordazsuay@gmail.com>
2022-09-03 18:45:11 +0200

    primer commit, subir base html y primeras funciones para añadir tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,61 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import poweredBy from "./powered-by-vitawind-dark.png";
+import { useEffect, useState } from "react";
+import logo from "./tarea-completada.png";
+import ListaTareas from "./components/listaTareas"
+
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState([]);
+  const [todos, setTodos] = useState([]);
+
+  function handleChange(event) {
+  const value = event.target.value
+    setTitle(value)
+    
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    const current = new Date();
+    const generateDate = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()} - ${current.getHours()}:${current.getMinutes()}`;
+    const newTodo = {
+        id: crypto.randomUUID(),
+        title: title,
+        complete: false,
+        date: generateDate
+        
+    }
+    const temp = [...todos];
+    temp.unshift(newTodo);
+
+    setTodos(temp);
+
+  }
+
 
   return (
-    <div className="text-center selection:bg-green-900">
-      <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-white">
-        <img
+   
+      <header className="flex min-h-screen flex-col items-center justify-top bg-[#f4f4f4] text-white">
+   
+  <div className="container py-16 mx-auto text-center sm:px-4 sm:py-16">
+    
+<div className="flex items-center justify-center text-white">
+     <img
           src={logo}
-          className="animate-speed h-60 motion-safe:animate-spin"
+          className="w-16 h-16 mb-10 animate-wave"
           alt="logo"
         />
-        <style>
-          {
-            "\
-            .animate-speed{\
-              animation-duration:20s;\
-            }\
-          "
-          }
-        </style>
-        <p className="bg-gradient-to-r from-emerald-300 to-sky-300 bg-clip-text text-5xl font-black text-transparent selection:bg-transparent">
-          Vite + React + Tailwindcss v3
-        </p>
-        <p className="mt-3">
-          <button
-            type="button"
-            className="my-6 rounded bg-gray-300 px-2 py-2 text-[#282C34] transition-all hover:bg-gray-200"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code className="text-[#8d96a7]">App.jsx</code> and save to test
-          HMR updates.
-        </p>
-        <p className="mt-3 flex gap-3 text-center text-[#8d96a7]">
-          <a
-            className="text-[#61dafb] transition-all hover:text-blue-400"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="text-[#61dafb] transition-all hover:text-blue-400"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-        <img src={poweredBy} className="mx-auto my-8" alt="powered-by" />
+        </div>
+
+        <h1 class="text-4xl font-extrabold leading-10 tracking-tight text-black sm:text-5xl sm:leading-none md:text-6xl xl:text-7xl"><span class="block">Lista de Tareas</span> <span class="text-5xl lg:text-7xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-blue-600 to-blue-800">¡Para organizar tu vida!</span></h1>
+        <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 relative sm:text-5xl">
+	
+		</h1>
+        </div>
+        <ListaTareas></ListaTareas>  
+    <p class="italic font-semibold text-center text-black">Desarrollado por <a target="_blank" className="underline font-bold" href="https://portfolio.joseordaz.com">Jose Ordaz</a> con mucho <svg class="inline-block w-12 h-12" viewBox="0 0 72 72" xmlns="http://www.w3.org/2000/svg"><path fill="#EA5A47" d="M59.5 25c0-6.9036-5.5964-12.5-12.5-12.5-4.7533 0-8.8861 2.6536-11 6.5598C33.8861 15.1536 29.7533 12.5 25 12.5c-6.9036 0-12.5 5.5964-12.5 12.5 0 2.9699 1.0403 5.6942 2.7703 7.8387l-.0043.0034L36 58.5397l20.7339-25.6975-.0043-.0034C58.4597 30.6942 59.5 27.9699 59.5 25z"></path><path fill="none" stroke="#000" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" d="M59.5 25c0-6.9036-5.5964-12.5-12.5-12.5-4.7533 0-8.8861 2.6536-11 6.5598C33.8861 15.1536 29.7533 12.5 25 12.5c-6.9036 0-12.5 5.5964-12.5 12.5 0 2.9699 1.0403 5.6942 2.7703 7.8387l-.0043.0034L36 58.5397l20.7339-25.6975-.0043-.0034C58.4597 30.6942 59.5 27.9699 59.5 25z"></path></svg></p>
+        
       </header>
-    </div>
+  
   );
 }
 
---
+++ src/components/listaTareas.jsx
@@ -0,0 +1,240 @@
+import React from "react";
+import { useState } from "react";
+import logoPapelera from "../papelera-de-reciclaje.svg";
+import logoJose from "../jose-ordaz.jpg";
+
+export const ListaTareas = ({onDelete}) => {
+  const [title, setTitle] = useState([]);
+  const [tareas, setTareas] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+
+  function handleChange(event) {
+    const value = event.target.value;
+    setTitle(value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const current = new Date();
+    const generateDate = `${current.getDate()}/${
+      current.getMonth() + 1
+    }/${current.getFullYear()} - ${current.getHours()}:${current.getMinutes()}`;
+    const newTareas = {
+      id: crypto.randomUUID(),
+      title: title,
+      complete: false,
+      date: generateDate,
+    };
+    const temp = [...tareas];
+    temp.unshift(newTareas);
+
+    setTareas(temp);
+  }
+
+  function FormEdit() {
+    return (
+      <a href="#" className="text-indigo-600 hover:text-indigo-900">
+        <button onClick={() => setIsEdit(item.id)}>Actualizar</button>
+      </a>
+    );
+  }
+
+  function handleDelete(id){
+    const temp = tareas.filter((item) => item.id !== id);
+    setTareas(temp);
+  }
+
+  return (
+    <div className="container mx-auto max-w-3xl px-4 sm:px-8">
+      <div className="py-8">
+        <div className="mb-1 flex w-full flex-row justify-between sm:mb-0">
+          <div className="text-end">
+            <form
+              onSubmit={handleSubmit}
+              className="flex w-3/4 max-w-sm flex-col justify-center space-y-3 md:w-full md:flex-row md:space-x-3 md:space-y-0"
+            >
+              <div className=" relative ">
+                <input
+                  type="text"
+                  id='"form-subscribe-Filter'
+                  className=" w-full flex-1 appearance-none rounded-lg border border-transparent border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600"
+                  placeholder="Escriba tu tarea..."
+                  value={title}
+                  onChange={handleChange}
+                />
+              </div>
+              <button
+                onClick={handleSubmit}
+                className="flex-shrink-0 rounded-lg bg-black px-4 py-2 text-base font-semibold text-white shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-purple-200"
+                type="submit"
+              >
+                Añadir
+              </button>
+            </form>
+          </div>
+          <div className="text-end">
+            <form className="flex w-3/4 max-w-sm flex-col justify-center space-y-3 md:w-full md:flex-row md:space-x-3 md:space-y-0">
+              <div className=" relative ">
+                <input
+                  type="text"
+                  id='"form-subscribe-Filter'
+                  className=" w-full flex-1 appearance-none rounded-lg border border-transparent border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600"
+                  placeholder="Buscar tarea..."
+                />
+              </div>
+              <button
+                className="flex-shrink-0 rounded-lg bg-black px-4 py-2 text-base font-semibold text-white shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-purple-200"
+                type="submit"
+              >
+                Buscar
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="-mx-4 overflow-x-auto px-4 py-4 sm:-mx-8 sm:px-8">
+          <div className="inline-block min-w-full overflow-hidden rounded-lg shadow">
+            <table className="min-w-full leading-normal">
+              <>
+                <thead>
+                  <tr>
+                    <th
+                      scope="col"
+                      className="border-b border-gray-200 bg-white  px-5 py-3 text-left  text-sm font-normal uppercase text-gray-800"
+                    >
+                      Usuario
+                    </th>
+                    <th
+                      scope="col"
+                      className="border-b border-gray-200 bg-white  px-5 py-3 text-left  text-sm font-normal uppercase text-gray-800"
+                    >
+                      Tarea
+                    </th>
+                    <th
+                      scope="col"
+                      className="border-b border-gray-200 bg-white  px-5 py-3 text-left  text-sm font-normal uppercase text-gray-800"
+                    >
+                      Fecha
+                    </th>
+                    <th
+                      scope="col"
+                      className="border-b border-gray-200 bg-white  px-5 py-3 text-left  text-sm font-normal uppercase text-gray-800"
+                    >
+                      Estado
+                    </th>
+                    <th
+                      scope="col"
+                      className="border-b border-gray-200 bg-white  px-5 py-3 text-left  text-sm font-normal uppercase text-gray-800"
+                    ></th>
+                    <th
+                      scope="col"
+                      className="border-b border-gray-200 bg-white  px-5 py-3 text-left  text-sm font-normal uppercase text-gray-800"
+                    ></th>
+                  </tr>
+                </thead>
+                {tareas.map((item) => (
+                  <tbody key={item.id}>
+                    <tr>
+                      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                        <div className="flex items-center">
+                          <div className="flex-shrink-0">
+                            <a href="#" className="relative block">
+                              <img
+                                alt="profil"
+                                src={logoJose}
+                                className="mx-auto h-10 w-10 rounded-full object-cover "
+                              />
+                            </a>
+                          </div>
+                          <div className="ml-3">
+                            <p className="whitespace-no-wrap text-gray-900">
+                              Jose Ordaz
+                            </p>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                        <p className="whitespace-no-wrap uppercase text-gray-900">
+                          <strong key={item.id}>{item.title}</strong>
+                        </p>
+                      </td>
+                      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                        <p
+                          key={item.id}
+                          className="whitespace-no-wrap text-gray-900"
+                        >
+                          {item.date}
+                        </p>
+                      </td>
+                      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                        <span className="relative inline-block px-3 py-1 font-semibold leading-tight text-green-900">
+                          <span className="absolute inset-0 rounded-full bg-green-200 opacity-50"></span>
+                          <span className="relative">active</span>
+                        </span>
+                      </td>
+                      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                        <a
+                          href="#"
+                          className="text-indigo-600 hover:text-indigo-900"
+                        >
+                          <button onClick={() => setIsEdit(item.id)}>
+                            Editar
+                          </button>
+                        </a>
+                      </td>
+                      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                        <a
+                          href="#"
+                          className="text-red-600 hover:text-indigo-900"
+                        >
+                          <button onClick={() => handleDelete(item.id)}>
+                            <img
+                              src={logoPapelera}
+                              className="animate-wave  w-5"
+                              alt="papelera"
+                            />
+                          </button>
+                        </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                ))}
+              </>
+            </table>
+
+            {/*Paginador*/}
+            {/* <div className="px-5 bg-white py-5 flex flex-col xs:flex-row items-center xs:justify-between">
+                        <div className="flex items-center">
+                            <button type="button" className="w-full p-4 border text-base rounded-l-xl text-gray-600 bg-white hover:bg-gray-100">
+                                <svg width="9" fill="currentColor" height="8" className="" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z">
+                                    </path>
+                                </svg>
+                            </button>
+                            <button type="button" className="w-full px-4 py-2 border-t border-b text-base text-indigo-500 bg-white hover:bg-gray-100 ">
+                                1
+                            </button>
+                            <button type="button" className="w-full px-4 py-2 border text-base text-gray-600 bg-white hover:bg-gray-100">
+                                2
+                            </button>
+                            <button type="button" className="w-full px-4 py-2 border-t border-b text-base text-gray-600 bg-white hover:bg-gray-100">
+                                3
+                            </button>
+                            <button type="button" className="w-full px-4 py-2 border text-base text-gray-600 bg-white hover:bg-gray-100">
+                                4
+                            </button>
+                            <button type="button" className="w-full p-4 border-t border-b border-r text-base  rounded-r-xl text-gray-600 bg-white hover:bg-gray-100">
+                                <svg width="9" fill="currentColor" height="8" className="" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z">
+                                    </path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListaTareas;
