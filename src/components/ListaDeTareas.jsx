0ff2d37b7fef1019bcdbab7ee1b094c90a9ce1cb
JoseOrdaz <joseordazsuay@gmail.com>
2022-10-10 15:39:50 +0200

    Arreglar imagenes people

--- src/components/ListaDeTareas.jsx
+++ src/components/ListaDeTareas.jsx
@@ -11,12 +11,12 @@ const people = [
   {
     id: 1,
     name: "Jose Ordaz",
-    avatar: "./src/jose-ordaz.jpg",
+    avatar: "./src/public/jose-ordaz.jpg",
   },
   {
     id: 2,
     name: "Elena Martínez",
-    avatar: "./src/elena.jpg",
+    avatar: "./src/public/elena-martinez.jpg",
   },
 ];
 
